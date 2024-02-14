bfd8824daea255f9673663eaa9be4552c8bab3d6
waize <waizeshaikh33@gmail.com>
2024-02-14 17:21:51 +0500

    facebook

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(function(){
+    $(".see-more").click(function(){
+        $(".other-links").css("opacity","1")
+    });
+    $(".see-more").click(function(){
+        $(".other-links").css("opacity","0")
+    });
+});
