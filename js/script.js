b83bfa0f7cca8fd81145735cf86692b7d73d8049
waize <waizeshaikh33@gmail.com>
2024-02-20 12:48:35 +0500

    facebook

--- js/script.js
+++ js/script.js
@@ -1,8 +1,32 @@
+// ############ right-bar Code
 $(function(){
-    $(".see-more").click(function(){
-        $(".other-links").css("opacity","1")
+    $(".group-main").click(function(){
+        $(".card").css({
+            display:"block"
+        })
     });
-    $(".see-more").click(function(){
-        $(".other-links").css("opacity","0")
+
+    $(".card i").click(function(){
+        $(".card").css({
+            display:"none"
+        })
+    });
+
+    $(".end-to-end").click(function(){
+        $(".end-p").text("end-to-end encrypted")
+        $(".end-to-end").css("background","#265073");
+        $(".circle").css({
+            position:"relative",
+            left:"57%"
+        })
+    });
+
+    $(".end-to-end").dblclick(function(){
+        $(".end-p").text("Use end-to-end encrypted");
+        $(".end-to-end").css("background","transparent");
+        $(".circle").css({
+            position:"relative",
+            left:"0%"
+        })
     });
 });
